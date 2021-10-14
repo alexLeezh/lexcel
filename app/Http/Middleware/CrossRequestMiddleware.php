a098cd40c5e759673e3fa515fc13a7dfcb329313
“zhouxiaogong” <zhouxiaogong@shopex.cn>
2021-10-14 17:58:30 +0800

    cros

--- app/Http/Middleware/CrossRequestMiddleware.php
+++ app/Http/Middleware/CrossRequestMiddleware.php
@@ -17,17 +17,12 @@ class CrossRequestMiddleware
         // header('Access-Control-Allow-Headers: X-Requested-With,X_Requested_With');
 
         $response = $next($request);
-        $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
-        $allow_origin = [
-            'http://localhost:8000',
-        ];
-        if (in_array($origin, $allow_origin)) {
-            $response->header('Access-Control-Allow-Origin', $origin);
-            $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
-            $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
-            $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
-            $response->header('Access-Control-Allow-Credentials', 'true');
-        }
+        
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
+        $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
+        $response->header('Access-Control-Allow-Methods', '*');
+        $response->header('Access-Control-Allow-Credentials', 'true');
         return $response;
     }
 
